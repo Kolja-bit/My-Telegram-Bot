4dcbb2347b88d07427fb1b52a8d1dd071ea0f3d2
Wasilii Martynov <nishilov@yandex.ru>
2025-09-01 17:06:20 +0300

    Subscribers init

--- src/main/java/com/example/My_Telegram_Bot/bot/command/StartCommand.java
+++ src/main/java/com/example/My_Telegram_Bot/bot/command/StartCommand.java
@@ -1,5 +1,7 @@
 package com.example.My_Telegram_Bot.bot.command;
 
+import com.example.My_Telegram_Bot.model.Subscribers;
+import com.example.My_Telegram_Bot.repositories.PriceRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -9,10 +11,16 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import org.telegram.telegrambots.meta.api.objects.Update;
 @Service
-@AllArgsConstructor
+//@AllArgsConstructor
 @Slf4j
 public class StartCommand implements IBotCommand {
+    private  final PriceRepository priceRepository;
+
+    public StartCommand(PriceRepository priceRepository) {
+        this.priceRepository = priceRepository;
+    }
 
     @Override
     public String getCommandIdentifier() {
@@ -33,11 +41,18 @@ public class StartCommand implements IBotCommand {
                 Привет! Данный бот помогает отслеживать стоимость биткоина.
                 Поддерживаемые команды:
                  /get_price - получить стоимость биткоина
+                 /start - регистрация нового пользователя
                 """);
         try {
+            Subscribers subscribers = new Subscribers();
+            Long userId = message.getFrom().getId();
+            subscribers.setTelegramUserID(userId);
+            subscribers.setUserSubscriptionPrice("null");
+            priceRepository.save(subscribers);
             absSender.execute(answer);
         } catch (TelegramApiException e) {
             //log.error("Error occurred in /start command", e);
         }
     }
+
 }
--- src/main/java/com/example/My_Telegram_Bot/model/Subscribers.java
+++ src/main/java/com/example/My_Telegram_Bot/model/Subscribers.java
@@ -1,23 +1,56 @@
 package com.example.My_Telegram_Bot.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.UUID;
 
-@Getter
-@Setter
+//@Getter
+//@Setter
 @Entity
 @Table(name = "Subscribers")
 public class Subscribers {
     @Id
-    @Column
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    @Column
-    //@GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "uuidUser")
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID uuidUser;
-    @Column(nullable = true)
-    private Double userSubscriptionPrice;
+    @Column(name = "telegramUserID")
+    private Long telegramUserID;
+    @Column(name = "userSubscriptionPrice", nullable = true)
+    private String userSubscriptionPrice;
+
+    public UUID getUuidUser() {
+        return uuidUser;
+    }
+
+    public void setUuidUser(UUID uuidUser) {
+        this.uuidUser = uuidUser;
+    }
+
+    public Long getTelegramUserID() {
+        return telegramUserID;
+    }
+
+    public void setTelegramUserID(Long telegramUserID) {
+        this.telegramUserID = telegramUserID;
+    }
+
+    public String getUserSubscriptionPrice() {
+        return userSubscriptionPrice;
+    }
+
+    public void setUserSubscriptionPrice(String userSubscriptionPrice) {
+        this.userSubscriptionPrice = userSubscriptionPrice;
+    }
 }
+    /*@bot.message_handler(commands=['start'])
+    def start_message(message):
+        your_variable = message.from_user.id*/
+
+/*import org.telegram.telegrambots.meta.api.objects.Update;
+public void onUpdateReceived(Update update) {
+        Long user_id = update.getMessage().getFrom().getId();
+        // Далее можно использовать user_id для проверки подписки на канал и выполнения других операций
+        }*/
